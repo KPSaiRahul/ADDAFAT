1e99a5f7a578c4042d02bd938dd59f5ba20d1099
KPSaiRahul <sairahulkp3@Gmail.com>
2024-04-29 18:21:52 +0530

    made changes to fib

--- fib.py
+++ fib.py
@@ -1,16 +1,16 @@
 def fibonacci(n):
     """
     Generate the Fibonacci sequence up to the nth term.
-    
+
     Args:
         n (int): The number of terms to generate in the Fibonacci sequence.
-        
+
     Returns:
         list: A list containing the Fibonacci sequence up to the nth term.
     """
     if n <= 0:
         return []
-    elif n == 1:
+    if n == 1:
         return [0]
     else:
         sequence = [0, 1]
@@ -18,6 +18,7 @@ def fibonacci(n):
             next_term = sequence[i - 1] + sequence[i - 2]
             sequence.append(next_term)
         return sequence
+
 # Generate the first 10 terms of the Fibonacci sequence
 fib_sequence = fibonacci(10)
 print(fib_sequence)  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
