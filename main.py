e819db4b306c56a75a4dfa802f44ac0572adce21
KPSaiRahul <sairahulkp3@Gmail.com>
2024-04-29 18:08:00 +0530

    added adding two numbers in python

---
+++ main.py
@@ -0,0 +1,12 @@
+# sum_of_two_numbers.py
+
+def calculate_sum(num1, num2):
+    
+    return num1 + num2
+
+# Example usage
+num1 = 5
+num2 = 7
+
+result = calculate_sum(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
